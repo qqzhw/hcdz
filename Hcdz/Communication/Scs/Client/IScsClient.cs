69936a8dc50f455e172934166eb56aeefa023730
ZHW <ZHW@DESKTOP-RHT79OF>
2018-01-11 17:48:41 +0800

    添加项目文件。

---
+++ Hcdz/Communication/Scs/Client/IScsClient.cs
@@ -0,0 +1,13 @@
+﻿using Hcdz.Communication.Scs.Communication;
+using Hcdz.Communication.Scs.Communication.Messengers;
+
+namespace Hcdz.Communication.Scs.Client
+{
+    /// <summary>
+    /// Represents a client to connect to server.
+    /// </summary>
+    public interface IScsClient : IMessenger, IConnectableClient
+    {
+        //Does not define any additional member
+    }
+}
